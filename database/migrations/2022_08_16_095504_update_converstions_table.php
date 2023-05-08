9c9001d418a75daa9cad778864042885f9b6004e
zahiqadan <117361140+zahiqadan@users.noreply.github.com>
2023-05-09 02:36:11 +0300

    tssa

--- database/migrations/2022_08_16_095504_update_converstions_table.php
+++ database/migrations/2022_08_16_095504_update_converstions_table.php
@@ -16,7 +16,7 @@ class UpdateConverstionsTable extends Migration
         Schema::table('conversations', function (Blueprint $table) {
             $table->renameColumn('user_id','sender_id');
 //            $table->string('sender_type');
-            $table->foreignId('receiver_id');
+//            $table->foreignId('receiver_id');
             $table->string('receiver_type');
             $table->foreignId('last_message_id')->nullable();
             $table->timestamp('last_message_time')->nullable();
@@ -38,7 +38,7 @@ class UpdateConverstionsTable extends Migration
         Schema::table('conversations', function (Blueprint $table) {
             $table->renameColumn('sender_id', 'user_id');
             $table->string('message');
-            $table->dropColumn('receiver_id');
+//            $table->dropColumn('receiver_id');
             $table->dropColumn('receiver_type');
             $table->dropColumn('last_message_id')->nullable();
             $table->dropColumn('last_message_time')->nullable();
