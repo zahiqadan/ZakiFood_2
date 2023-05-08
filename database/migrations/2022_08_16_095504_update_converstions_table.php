e9d31394b22b92f869007da7a70e7c284d5ca412
zahiqadan <117361140+zahiqadan@users.noreply.github.com>
2023-05-09 02:37:31 +0300

    tssa

--- database/migrations/2022_08_16_095504_update_converstions_table.php
+++ database/migrations/2022_08_16_095504_update_converstions_table.php
@@ -17,7 +17,7 @@ class UpdateConverstionsTable extends Migration
             $table->renameColumn('user_id','sender_id');
 //            $table->string('sender_type');
 //            $table->foreignId('receiver_id');
-            $table->string('receiver_type');
+//            $table->string('receiver_type');
             $table->foreignId('last_message_id')->nullable();
             $table->timestamp('last_message_time')->nullable();
             $table->integer('unread_message_count')->default(0);
@@ -39,7 +39,7 @@ class UpdateConverstionsTable extends Migration
             $table->renameColumn('sender_id', 'user_id');
             $table->string('message');
 //            $table->dropColumn('receiver_id');
-            $table->dropColumn('receiver_type');
+//            $table->dropColumn('receiver_type');
             $table->dropColumn('last_message_id')->nullable();
             $table->dropColumn('last_message_time')->nullable();
 //            $table->dropColumn('sender_type');
